89d6f830e4e7dc97a0b37c4bf4f0e00649f28638
IAFPhantom <43669866+IAFPhantom@users.noreply.github.com>
2018-10-19 18:39:25 +0300

    F-4E / 2000 cockpits working

---
+++ Mods/aircraft/IAF_Flyablepack_A10A/FM/FMOptionsMiG29.lua
@@ -0,0 +1,15 @@
+deltaCntrB			= 0.0
+wingMzDampK			= 0.3
+fuselageMyDampK		= 0.3
+fuselageMzDampK		= 0.2
+
+mainGearAreaAD		= 0.43
+noseGearAreaAD		= 0.2
+
+engine_p_y			= -0.411
+
+stbsk_k_aoa			= 1.0
+stbsk_k_1			= 1.0
+stbsk_pwr			= 1.5
+
+op_hyd_sp_k			= 1.0
