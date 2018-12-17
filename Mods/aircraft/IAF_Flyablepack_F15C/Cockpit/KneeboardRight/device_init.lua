3803b27f5fdb2eab9a125bca84e4a7fb6ee74b2e
IAFPhantom <43669866+IAFPhantom@users.noreply.github.com>
2018-12-17 22:41:00 +0200

    Update device_init.lua

--- Mods/aircraft/IAF_Flyablepack_F15C/Cockpit/KneeboardRight/device_init.lua
+++ Mods/aircraft/IAF_Flyablepack_F15C/Cockpit/KneeboardRight/device_init.lua
@@ -1,11 +1,9 @@
-dofile('Mods/aircraft/Flaming Cliffs/Cockpit/KneeboardRight/device_init.lua')
+mount_vfs_texture_archives("Bazar/Textures/AvionicsCommon")
 
----mount_vfs_texture_archives("Bazar/Textures/AvionicsCommon")
-
---attributes = {
---	"support_for_cws",
---}
+attributes = {
+	"support_for_cws",
+}
 ---------------------------------------------
---dofile(LockOn_Options.common_script_path.."KNEEBOARD/declare_kneeboard_device.lua")
+dofile(LockOn_Options.common_script_path.."KNEEBOARD/declare_kneeboard_device.lua")
 ---------------------------------------------
 
